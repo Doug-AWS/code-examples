0d67221e257184b847d006fc3bc4333abfcc62a2
Doug Schwartz <dougsch@amazon.com>
2021-01-19 07:01:36 -0800

    Added content to dummy go test

---
+++ go/dummy/dummy_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDummy(t *testing.T) {
+	thisTime := time.Now()
+	nowString := thisTime.Format("2006-01-02 15:04:05 Monday")
+	t.Log("Starting unit test at " + nowString)
+
+	t.Log("Test complete")
+}

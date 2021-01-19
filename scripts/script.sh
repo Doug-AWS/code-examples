3b1d5ec8e00fbc9066823a5e923819cb43ee8458
Doug Schwartz <dougsch@amazon.com>
2021-01-19 08:49:32 -0800

    Updated script to enable running other language unit tests

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,23 +1,30 @@
 TestGoFile () {
-    if [ "$1" != "" ]
+    if [ "$1" == "" ]
     then
-	path="$(dirname $1)"
-        file="$(basename $1)"
-	pushd $path
-
-	declare RESULT=(`go test`)  # (..) = array
-	
-	if [ "${RESULT[0]}" == "PASS" ]
-	then
-	    echo 0
-	else
-	    echo 1
-	fi
-	popd
+       return
     fi
+
+    pushd $1
+
+    declare RESULT=(`go test`)  # (..) = array
+    
+    if [ "${RESULT[0]}" == "PASS" ]
+    then
+      echo 0
+    else
+      echo 1
+    fi
+
+    popd
 }
 
 for f in $@ ; do
     # Do any end with "_test.go"?
-    [[ $f =~ ^[a-zA-Z/]*_test.go$ ]] && TestGoFile "$f"
+    path="$(dirname $f)"
+    file="$(basename $f)"
+    echo Looking at $file in $path
+
+    # If it's a go test file
+    # test it
+    [[ $file =~ ^[a-zA-Z]*_test.go$ ]] && TestGoFile "$path"
 done

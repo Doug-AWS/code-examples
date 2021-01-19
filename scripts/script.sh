85ebacb2f1991623b52e65d31766bc7128a37040
Doug Schwartz <dougsch@amazon.com>
2021-01-19 07:17:39 -0800

    Updated checkin script to run go test

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,7 +1,23 @@
-# echo args
-for f in "$@" ; do
-    if [ "$f" == "*_test.go" ]
+TestGoFile () {
+    if [ "$1" != "" ]
     then
-       echo $f
+	path="$(dirname $1)"
+        file="$(basename $1)"
+	pushd $path
+
+	declare RESULT=(`go test`)  # (..) = array
+	
+	if [ "${RESULT[0]}" == "PASS" ]
+	then
+	    echo 0
+	else
+	    echo 1
+	fi
+	popd
     fi
+}
+
+for f in $@ ; do
+    # Do any end with "_test.go"?
+    [[ $f =~ ^[a-zA-Z/]*_test.go$ ]] && TestGoFile "$f"
 done
